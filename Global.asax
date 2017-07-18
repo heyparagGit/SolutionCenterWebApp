c914e37f77398491f7f1c9864b5269be66fe24b9
heyparaggit <heyparag@gmail.com>
2017-07-18 14:53:25 -0700

    Created Web application Template

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SolutionCenterWebApp.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SolutionCenterWebApp.Startup))]
+namespace SolutionCenterWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
